e69ab39a12b85029a57ff96798f47a21fcf7f373
Serg4356 <asdfg111000@yandex.ru>
2017-11-24 17:27:53 +0500

    Done 2

--- pprint_json.py
+++ pprint_json.py
@@ -1,23 +1,16 @@
 import json
-from urllib import request
+import sys
 
 
 def load_data(filepath):
-    page = request.urlopen(filepath)
-    lines = []
-    for line in page.readlines():
-        lines.append(line)
-    page.close()
-    for i in range(len(lines)):
-        lines[i] = lines[i].decode('utf-8')
-    data = ''.join(lines)
+    data = open(filepath,'r').read()
     return data
 
 
 def pretty_print_json(data):
     jdata = json.loads(data)
-    return json.dumps(jdata, sort_keys=True, indent=4, separators=(',', ': '))
+    return json.dumps(jdata, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    print(pretty_print_json(load_data('https://devman.org/media/filer_public/1d/32/1d32132e-efa4-4a6c-bd32-312acc3710ad/alco_shops.json')))
+    print(pretty_print_json(load_data(sys.argv[1])))
