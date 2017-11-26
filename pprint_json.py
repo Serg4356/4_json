a24dfa21cfc25ffc8db8cec3e8eb0dd24badd9e7
Serg4356 <asdfg111000@yandex.ru>
2017-11-26 13:38:17 +0500

    Change variable name, add with const.

--- pprint_json.py
+++ pprint_json.py
@@ -3,13 +3,14 @@ import sys
 
 
 def load_data(filepath):
-    unstruckted_data = json.loads(open(filepath,'r').read())
+    with open(filepath,'r') as opened_file:
+        unstruckted_data = json.loads(opened_file.read())
     return unstruckted_data
 
 
-def pretty_print_json(unstruckted_data):
+def prettify_json(unstruckted_data):
     return json.dumps(unstruckted_data, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    print(pretty_print_json(load_data(sys.argv[1])))
+    print(prettify_json(load_data(sys.argv[1])))
